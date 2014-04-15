8ad06f956dfa9e806b337427f093d0234efd3cc8
SimkaDev <sion.romain@live.fr>
2014-04-15 14:22:57 +0200

    Ajout d'un livre- auteur et type au lancement de l'app

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import biblioj.Auteur
+import biblioj.Livre
+import biblioj.TypeDocument
+
 class BootStrap {
 
     def init = { servletContext ->
+		Auteur a = new Auteur( nom : "Voltaire" , prenom : "d").save()
+		TypeDocument t = new TypeDocument (intitule : "Roman").save()
+		Livre l = new Livre (titre : "Candide" , nombreExemplaires : 5 , nombreExemplairesDisponibles : 0 , auteurs : a, type : t ).save()
     }
     def destroy = {
     }
