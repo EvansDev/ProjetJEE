eb9e0964b95fde35fcc2194d6d7e187464617cab
Evans <Evans@130.120.209.110>
2014-04-15 14:35:17 +0200

    BootStrap init

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import biblioj.Auteur
+import biblioj.Livre
+import biblioj.TypeDocument
+
 class BootStrap {
 
     def init = { servletContext ->
+		TypeDocument nouveaute = new TypeDocument (intitule : "Nouveauté")
+		nouveaute.save()
+		Auteur vigian = new Auteur(nom : "Vigian", prenom : "Delphine de")
+		vigian.save()
+		Livre livre1 = new Livre (titre:"Rien ne s'oppose à la nuit : roman", nombreExemplaires : 2,
+										nombreExemplairesDisponibles: 2, type: nouveaute, auteur : vigian)
+		livre1.save()
+		Vigian.addToLivres(livre1)
+		
     }
     def destroy = {
     }
