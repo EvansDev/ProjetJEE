0e8f9ec44049e65916612bf6ca144f75c06c09db
EvansDev <evans-belloeil@hotmail.fr>
2014-04-17 10:22:49 +0200

    Fin creation dans bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -29,8 +29,32 @@ class BootStrap {
 										nombreExemplairesDisponibles: 2, type: nouveaute, auteurs : vigian)
 		Livre livre2 = new Livre (titre:"Hunger Games", nombreExemplaire : 10,
 										nombreExemplaireDisponible: 10, type: livreAdo, auteur : collins )
+		Livre livre3 = new Livre (titre:"La couleur des sentiments", nombreExemplaire : 8,
+										nombreExemplaireDisponible: 8, type: nouveaute, auteur : stockett)
+		Livre livre4 = new Livre (titre:"L'embrasement", nombreExemplaire : 10,
+										nombreExemplaireDisponible: 10, type: livreAdo, auteur : collins)
+		Livre livre5 = new Livre (titre:"Limonov", nombreExemplaire : 5,
+										nombreExemplaireDisponible: 5, type: nouveaute, auteur : carrere)
+		Livre livre6 = new Livre (titre:"1Q84. 1. Avril-juin", nombreExemplaire : 4,
+										nombreExemplaireDisponible: 4, type: nouveaute, auteur : murakami)
+		Livre livre7 = new Livre (titre:"1Q84. 3. Octobre-décembre", nombreExemplaire : 4,
+										nombreExemplaireDisponible: 4, type: nouveaute, auteur : murakami)
+		Livre livre8 = new Livre (titre:"La révolte", nombreExemplaire : 10,
+										nombreExemplaireDisponible: 10, type: livreAdo, auteur : collins)
+		Livre livre9 = new Livre (titre:"La liste de mes envies", nombreExemplaire : 6,
+										nombreExemplaireDisponible: 6, type: livreAdulte, auteur : delacourt)
+		Livre livre10 = new Livre (titre:"1Q84. 2. Juillet-septembre", nombreExemplaire : 4,
+										nombreExemplaireDisponible: 4, type: nouveaute, auteur : murakami)
 		livre1.save()
 		livre2.save()
+		livre3.save()
+		livre4.save()
+		livre5.save()
+		livre6.save()
+		livre7.save()
+		livre8.save()
+		livre9.save()
+		livre10.save()
     }
     def destroy = {
     }
