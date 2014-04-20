d8e3ec40a1f6e1c51ef19fe9c50256e418dc24a4
EvansDev <evans-belloeil@hotmail.fr>
2014-04-20 22:34:30 +0200

    Reservation Test

--- test/unit/biblioj/ReservationTests.groovy
+++ test/unit/biblioj/ReservationTests.groovy
@@ -3,6 +3,7 @@ package biblioj
 
 
 import grails.test.mixin.*
+
 import org.junit.*
 
 /**
@@ -11,7 +12,36 @@ import org.junit.*
 @TestFor(Reservation)
 class ReservationTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+	TypeDocument type 
+	Livre livre 
+	Reservation res 
+	Reservation res2
+	@Before
+	void setUp(){
+		type = new TypeDocument( intitule : "Livre Ado" )
+		livre = new Livre( titre : "testLivre", type : type, nombreExemplaires : 2, nombreExemplairesDisponibles : 2 )
+		res = new Reservation( code : 1, dateReservation : new Date("24/04/2014") )
+		res2 = new Reservation( code : "", dateReservation: "" )
+	}
+	
+	@After
+	void tearsDown(){
+		type = null
+		livre = null
+		res = null
+	}
+	
+    void testCreation() {
+		assert res != null
+		assert res.save() != null
+	}
+
+	void testMauvaiseCreation() {
+		assert res2 != null
+		assert res2.save() == null
+	}
+
+	void testToString() {
+		assert res.toString() == "1 - Fri Dec 04 00:00:00 CET 2015"
+	}
 }
